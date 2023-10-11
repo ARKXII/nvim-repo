a05c601edec35331fc4d0e6cf8ca9b304dc9708f
Joshua Villalobos <80900228+ARKXII@users.noreply.github.com>
2023-10-11 12:03:18 +0800

    nvim backup

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,88 @@
+local plugins = {
+   --fzf issue   -- ChatGPT
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "gpg --decrypt C:/Users/Joshua/.password-store/home.gpg",
+      })
+    end,
+  },
+  --python things
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+        "ruff",
+        "black",
+        "mypy",
+        "debugpy",
+        "fzf",
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  "vim-test/vim-test",
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function (_, opts)
+      local path = "$HOME/AppData/Local/nvim-data/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function ()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function ()
+        dapui.open()
+      dap.listeners.before.event_terminated["dapui_config"] = function ()
+          dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function ()
+          dapui.close()
+      end
+    end
+  end
+  },
+}
+
+return plugins
+
